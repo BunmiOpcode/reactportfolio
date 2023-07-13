4e3e6c73c1f1ca1a1f724852a52665c0ee1b5c07
BunmiOpcode <bellobunmi123@gmail.com>
2023-07-13 10:17:46 +0100

    header

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header-wrapper'>
+       <div className='main'>
+         <h1>web development and website promotions</h1>   
+      </div>
+    </div>
+  )
+}
+
+export default Header
